29b429678cc436eedd9495c41531342d7642aebb
GabrielIbujes <gaboibujes@gmail.com>
2020-07-28 15:57:21 -0500

    primera parte

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,6 @@ use Illuminate\Support\Facades\Route;
         Route::get('articles/{article}/image','ArticleController@image');
 
 
-
-
         //Articles
         //Route::get('user', 'UserController@getAuthenticatedUser');
         Route::get('articles/{article}', 'ArticleController@show');
